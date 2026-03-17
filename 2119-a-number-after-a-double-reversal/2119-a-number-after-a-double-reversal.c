2df917703d8dc4a12193ffe8ab995e35ab5f90dc
Resin R <resinr27@gmail.com>
2026-03-17 13:04:32 +0530

    Time: 0 ms (100%), Space: 8.4 MB (14.76%) - LeetHub

---
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.c
@@ -0,0 +1,11 @@
+#include<stdbool.h>
+bool isSameAfterReversals(int num) {
+    if(num==0){
+        return true;
+    }
+    if(num%10==0){
+        return false;
+    }
+    return true;
+    }
+    

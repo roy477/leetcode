5c64ef94b32b831ac2555df7ed13ffbfc3a74e20
Resin R <resinr27@gmail.com>
2026-03-17 12:53:26 +0530

    Time: 0 ms (100%), Space: 8.9 MB (5.92%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.c
@@ -0,0 +1,16 @@
+#include <stdbool.h>
+
+bool check(int* nums, int numsSize) {
+
+    int count = 0;
+
+    for(int i = 0; i < numsSize; i++)
+    {
+        if(nums[i] > nums[(i + 1) % numsSize])
+        {
+            count++;
+        }
+    }
+
+    return count <= 1;
+}

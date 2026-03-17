a96c569516919295c5810eb1d23e232383b52468
Resin R <resinr27@gmail.com>
2026-03-17 12:56:28 +0530

    Time: 0 ms (100%), Space: 11.3 MB (20.79%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.c
@@ -0,0 +1,15 @@
+struct ListNode* reverseList(struct ListNode* head) {
+    
+    struct ListNode* prev = NULL;
+    struct ListNode* curr = head;
+    struct ListNode* next = NULL;
+
+    while(curr != NULL) {
+        next = curr->next;   // store next node
+        curr->next = prev;   // reverse link
+        prev = curr;         // move prev forward
+        curr = next;         // move curr forward
+    }
+
+    return prev;
+}

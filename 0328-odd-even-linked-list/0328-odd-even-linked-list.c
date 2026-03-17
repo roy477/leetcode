597027bea1bf25ef00b4a7cb984d1b879e0d7197
Resin R <resinr27@gmail.com>
2026-03-17 12:51:08 +0530

    Time: 0 ms (100%), Space: 12 MB (22.11%) - LeetHub

---
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.c
@@ -0,0 +1,22 @@
+struct ListNode* oddEvenList(struct ListNode* head) {
+
+    if(head == NULL || head->next == NULL)
+        return head;
+
+    struct ListNode* odd = head;
+    struct ListNode* even = head->next;
+    struct ListNode* evenHead = even;
+
+    while(even != NULL && even->next != NULL)
+    {
+        odd->next = even->next;
+        odd = odd->next;
+
+        even->next = odd->next;
+        even = even->next;
+    }
+
+    odd->next = evenHead;
+
+    return head;
+}
